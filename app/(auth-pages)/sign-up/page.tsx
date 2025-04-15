1b1925d5dd563e5aa550bd1adf7e206f5eae9114
jonathanporras <porrasjon@gmail.com>
2025-04-14 22:38:04 -0700

    Begin polishing sign up page

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -17,33 +17,44 @@ export default async function Signup(props: { searchParams: Promise<Message> }) 
   }
 
   return (
-    <>
-      <form className="flex flex-col min-w-64 max-w-64 mx-auto">
-        <h1 className="text-2xl font-medium">Sign up</h1>
-        <p className="text-sm text text-foreground">
-          Already have an account?{" "}
-          <Link className="text-primary font-medium underline" href="/log-in">
-            Sign in
-          </Link>
-        </p>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            minLength={6}
-            required
-          />
-          <SubmitButton formAction={signUpAction} pendingText="Signing up...">
-            Sign up
-          </SubmitButton>
-          <FormMessage message={searchParams} />
-        </div>
-      </form>
-      <SmtpMessage />
-    </>
+    <div className="flex flex-col md:flex-row w-full">
+      <div className="md:w-3/4 mb-40 w-full p-4">
+        <form className="flex flex-col min-w-64 mx-auto max-w-2xl my-20">
+          <h1 className="text-2xl font-medium">
+            Almost Done! Create an Account to Save Your Document
+          </h1>
+          <p className="text-sm text text-foreground">
+            Already have an account?{" "}
+            <Link className="text-primary font-medium underline" href="/log-in">
+              Sign in
+            </Link>
+          </p>
+          <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8 max-w-md">
+            <Label htmlFor="email">Email</Label>
+            <Input name="email" placeholder="you@example.com" required />
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              minLength={6}
+              required
+            />
+            <SubmitButton
+              className="bg-[#2FAF68] hover:bg-[#37c476] transition text-white px-4 py-2 rounded"
+              formAction={signUpAction}
+              pendingText="Signing up..."
+            >
+              Create My Account
+            </SubmitButton>
+            <FormMessage message={searchParams} />
+          </div>
+        </form>
+      </div>
+      <div className="md:w-1/4 w-full bg-gray-100 flex flex-col justify-center">
+        Your document Lease Agreement Ready to print Downloads as PDF and in Word Easy to
+        update
+      </div>
+    </div>
   );
 }
