e93207bdf7e61f501ed2689401148e4ca45abe1b
jonathanporras <porrasjon@gmail.com>
2025-04-15 21:48:02 -0700

    Fix type for form message on sign up page

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -12,7 +12,7 @@ export default async function Signup(props: {
   if ("message" in searchParams) {
     return (
       <div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
-        <FormMessage message={searchParams} />
+        <FormMessage message={searchParams as Message} />
       </div>
     );
   }
