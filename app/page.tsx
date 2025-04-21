bd0a5b450788ac8ce46039c521edd204f4ed3fd2
jonathanporras <porrasjon@gmail.com>
2025-04-21 09:01:22 -0700

    Redirect home route to dashboard if logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import LeaseAgreementLanding from "./lease-agreement/page";
 
 export default async function Home() {
-  return (
-    <>
-      <Hero />
-      <main>
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
-  );
+  return <LeaseAgreementLanding />;
 }
