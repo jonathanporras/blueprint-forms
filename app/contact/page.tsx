2aabc72063116ff07f8bbe1213cbede0cf4e8d17
jonathanporras <porrasjon@gmail.com>
2025-04-22 21:42:45 -0700

    Add color to email link on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,9 @@ export default async function Contact() {
   return (
     <div className="max-w-xl mx-auto px-4 my-20 flex flex-col items-center min-h-screen">
       <p className="font-bold text-xl">Contact us:</p>{" "}
-      <a href="mailto:support@quickformpro.com">support@quickformpro.com</a>
+      <a href="mailto:support@quickformpro.com" className="text-[#4285F4]">
+        support@quickformpro.com
+      </a>
     </div>
   );
 }
