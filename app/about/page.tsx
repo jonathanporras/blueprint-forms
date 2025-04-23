c00f59531b28315871f0560a1a3d6f9f25b66f3f
jonathanporras <porrasjon@gmail.com>
2025-04-22 21:37:44 -0700

    Fix typo on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 export default async function About() {
   return (
     <div className="max-w-xl mx-auto px-4 my-20 flex flex-col items-center min-h-screen">
-      <h3 className="font-bold text-xl pb-8">Welcome to QucikForm Pro!</h3>{" "}
+      <h3 className="font-bold text-xl pb-8">Welcome to QuickForm Pro!</h3>{" "}
       <p>
         QuickForm Pro helps business owners create, customize, and manage legal documents with
         ease. We simplify complex workflows and reduce drafting time. QuickForm Pro is ideal
