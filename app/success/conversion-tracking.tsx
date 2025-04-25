6f65da8f9df3a1477dc85d62c70fc201e20b339a
jonathanporras <porrasjon@gmail.com>
2025-04-24 21:58:13 -0700

    Remove explicit ga call from conversion tracking

--- app/success/conversion-tracking.tsx
+++ app/success/conversion-tracking.tsx
@@ -1,13 +1,10 @@
 "use client";
-import { sendGAEvent, sendGTMEvent } from "@next/third-parties/google";
+import { sendGTMEvent } from "@next/third-parties/google";
 import { useEffect } from "react";
 
 export default function ConversionTracking({ value }: { value: string }) {
   useEffect(() => {
     setTimeout(() => {
-      sendGAEvent("event", "purchase", {
-        value: value,
-      });
       sendGTMEvent({ event: "conversion", value: value });
     }, 1000);
   });
