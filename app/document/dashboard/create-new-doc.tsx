d7fa6a2af05705e2f17dcd4e3eb011d12950529a
jonathanporras <porrasjon@gmail.com>
2025-05-04 14:48:12 -0700

    Fix links to documents of dashboard

--- app/document/dashboard/create-new-doc.tsx
+++ app/document/dashboard/create-new-doc.tsx
@@ -1,9 +1,16 @@
+"use client";
+import { ArrowUpRight } from "lucide-react";
+
 export default function CreateNewDoc() {
   return (
     <div className="w-full border rounded flex flex-col p-4">
-      <h4 className="text-lg font-semibold mb-6">Create a New Document</h4>
-      <a href="/document/editor/lease-agreement" className="text-[#4285F4]">
-        &#8226; Lease Agreement
+      <h4 className="text-lg font-semibold mb-6">Create New Documents:</h4>
+      <a
+        href="/document/editor/lease-agreement"
+        className="bg-[#2FAF68] hover:bg-[#37c476] max-w-sm text-white px-1 py-3 rounded-lg transition flex justify-center items-center"
+      >
+        Create My Lease Agreement
+        <ArrowUpRight className="ml-2" size={18} />
       </a>
     </div>
   );
--- app/document/dashboard/page.tsx
+++ app/document/dashboard/page.tsx
@@ -26,36 +26,36 @@ const DocumentDashbord = async () => {
     <div className="min-h-screen pt-8 px-8">
       <div className="w-full mx-auto max-w-7xl py-6 px-4 md:px-10 bg-white border rounded">
         <h2 className="text-lg font-semibold mb-4">My Documents:</h2>
-        <div className="min-h-screen">
+        <div className="min-h-100">
           {documents.map((document: Document) => (
             <div
               key={document.id}
               className="flex md:flex-row flex-col md:justify-between justify-start gap-4 p-4 bg-white border-t"
             >
               <div className="flex-1 flex">
-                <Link
+                <a
                   href={`/document/editor/${document.template_type}/${document.id}`}
                   className="pr-4"
                 >
                   <ExternalLink className="text-[#2FAF68] hover:text-[#37c476]" />
-                </Link>
+                </a>
                 <DocumentName document={document} />
               </div>
               <div className="flex gap-2 md:justify-end justify-start">
-                <Link
+                <a
                   className="bg-[#2FAF68] hover:bg-[#37c476] text-white px-4 py-1 rounded-lg transition"
                   href={`/document/editor/${document.template_type}/${document.id}`}
                 >
                   <FilePenLine className="inline pr-2" />
                   Edit
-                </Link>
-                <Link
+                </a>
+                <a
                   className="bg-[#2FAF68] hover:bg-[#37c476] text-white px-4 py-1 rounded-lg transition"
                   href={`/document/editor/${document.template_type}/${document.id}`}
                 >
                   <FolderDown className="inline pr-2" />
                   Export
-                </Link>
+                </a>
                 {document?.id && <TwoStepDeleteButton documentId={document?.id} />}
               </div>
             </div>
